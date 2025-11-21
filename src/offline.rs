84fd36d241b7ef4f3916168fc42e6ac0358295b0
michael <66692526+mkxl@users.noreply.github.com>
2025-11-21 14:41:01 -0500

    updates

--- src/offline.rs
+++ src/offline.rs
@@ -14,7 +14,8 @@ impl Llm for Offline {
     fn stream_texts(&mut self, system_prompt: String, mut llm_inputs: Vec<LlmInput>) -> LlmStream<'_> {
         crate::llm::llm_stream! {
             let text_iter = llm_inputs.iter_mut().map(LlmInput::take_content_string);
-            let text_iter = system_prompt.once().chain(text_iter).intersperse("\n".to_owned());
+            let newline_iter = "\n".to_owned().repeat();
+            let text_iter = system_prompt.once().chain(text_iter).interleave_shortest(newline_iter);
 
             for text in text_iter {
                 yield text;

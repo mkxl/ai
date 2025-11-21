51fb6bc4c6d39b83eeffff07cee1e47b812ca76c
michael <66692526+mkxl@users.noreply.github.com>
2025-11-21 14:33:45 -0500

    updates

--- src/offline.rs
+++ src/offline.rs
@@ -3,20 +3,23 @@ use crate::{
     llm_input::LlmInput,
 };
 use derive_more::Constructor;
+use itertools::Itertools;
 use mkutils::Utils;
 
 #[derive(Constructor, Default)]
 pub struct Offline;
 
 impl Llm for Offline {
+    #[allow(unstable_name_collisions)]
     fn stream_texts(&mut self, system_prompt: String, llm_inputs: Vec<LlmInput>) -> LlmStream<'_> {
-        crate::llm::llm_stream! {
-            yield system_prompt;
-            yield "\n".to_owned();
+        let text_iter = llm_inputs
+            .into_iter()
+            .map(|mut llm_input| llm_input.take_content_string());
+        let text_iter = system_prompt.once().chain(text_iter).intersperse("\n".to_owned());
 
-            for mut llm_input in llm_inputs {
-                yield llm_input.take_content_string();
-                yield "\n".to_owned();
+        crate::llm::llm_stream! {
+            for text in text_iter {
+                yield text;
             }
         }
     }
